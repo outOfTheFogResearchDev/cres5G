4d2b794da64cce16fea1b697f1ea3ee4117bf5c4
Max-Berkowitz <maxl.berkowitz@gmail.com>
2021-05-24 16:44:30 -0400

    .bat file fixes 2

--- setupCresGen2Proto.bat
+++ setupCresGen2Proto.bat
@@ -4,5 +4,5 @@ mkdir config &
 echo exports.SECRET = `${Math.random()}`; > config/config.js &
 npm run setup &
 echo ^@echo off >> ../cresGen2Proto.bat &
-echo cd cresGen2Proto ^& >> ../cresGen2Proto.bat
+echo cd cresGen2Proto ^& >> ../cresGen2Proto.bat &
 echo startCresGen2Proto.bat >> ../cresGen2Proto.bat
